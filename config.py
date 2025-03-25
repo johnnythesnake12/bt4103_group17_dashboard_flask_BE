bf012de99a8ece37eb0892ae2ad076ed585ba587
johnnythesnake12 <98082700+johnnythesnake12@users.noreply.github.com>
2025-03-25 21:18:30 +0800

    changed to use pymysql

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     MYSQL_HOST = "localhost"  # or "127.0.0.1"
 
-    MYSQL_USER = "admin"
+    MYSQL_USER = "root"
     MYSQL_PASSWORD = "password"
     MYSQL_DB = "retimark_db"
 
