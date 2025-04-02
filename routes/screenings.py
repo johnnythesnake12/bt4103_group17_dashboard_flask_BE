f58cef5ae8373a28bd4c334209fee2ea9a1a57f6
johnnythesnake12 <98082700+johnnythesnake12@users.noreply.github.com>
2025-04-02 17:26:32 +0800

    Changed conn.cursor to use psycopg2

--- routes/screenings.py
+++ routes/screenings.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from pymysql.cursors import DictCursor
+import psycopg2.extras
 from db import get_db
 
 screenings_bp = Blueprint('screenings', __name__)
@@ -7,13 +7,13 @@ screenings_bp = Blueprint('screenings', __name__)
 @screenings_bp.route('/screenings', methods=['GET'])
 def get_screenings():
     conn = get_db()
-    cur = conn.cursor(DictCursor)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     try:
         cur.execute("SELECT * FROM Screenings")
         rows = cur.fetchall()
+        return jsonify([dict(row) for row in rows]) 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     finally:
         cur.close() 
     
-    return jsonify(rows)
